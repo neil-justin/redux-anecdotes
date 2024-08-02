d22a10b3f62ff85a6a9d9161b50e3b7af58702e2
Neil <mallarineil9@gmail.com>
2024-08-02 20:36:32 +0800

    Implement redux toolkit into filterReducer

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,12 +3,13 @@ import { filterChange } from "../reducers/filterReducer"
 
 const Filter = () => {
   const dispatch = useDispatch()
+
   return (
     <div>
       filter {' '}
       <input
         name="filter"
-        onChange={(event) => dispatch(filterChange(event.target.value))}
+        onChange={(event) => dispatch(filterChange({ substring: event.target.value }))}
       />
     </div>
   )
--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,19 +1,34 @@
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'FILTER_ANECDOTES':
-      return action.payload.substring
-    default: return state
-  }
-}
+// const filterReducer = (state = '', action) => {
+//   switch (action.type) {
+//     case 'FILTER_ANECDOTES':
+//       return action.payload.substring
+//     default: return state
+//   }
+// }
 
-const filterChange = (substring) => {
-  return {
-    type: 'FILTER_ANECDOTES',
-    payload: {
-      substring
+// const filterChange = (substring) => {
+//   return {
+//     type: 'FILTER_ANECDOTES',
+//     payload: {
+//       substring
+//     }
+//   }
+// }
+
+// export default filterReducer
+// export { filterChange }
+
+import { createSlice, current } from "@reduxjs/toolkit";
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    filterChange(state, action) {
+      return action.payload.substring
     }
   }
-}
+})
 
-export default filterReducer
-export { filterChange }
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
